0e924e56f8a33c6c42c0c34765792733d34b30c6
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-10-05 08:31:55 -0300

    att

--- aula2/questoes/24.c
+++ aula2/questoes/24.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
 int main(){
     char letra;
---
+++ aula3/getch.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Oi\n");
+    getch(); // O getch pausa a aplicação até receber um valor e quando recebe, ele não mostra na tela.
+    system("PAUSE");
+
+    return 0;
+}
