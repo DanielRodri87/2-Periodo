93d85797f1882df9f4a5e7aa9de59a0373d9692d
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-10-07 12:12:01 -0300

    att

---
+++ Atividades/capitulo3/questao14.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int i;
+    printf("\n\nIIIIIIIIIIII\n");
+    for (i =0; i < 8; i++)
+    {
+        printf("III\n");
+    }
+    printf("IIIIIIIIII\n");
+    for (i =0; i < 8; i++)
+    {
+        printf("III\n");
+    }
+    printf("IIIIIIIIIIII\n");
+    system("pause");
+    printf("\n");
+    return 0;
+}
--- Atividades/capitulo3/testes.c
+++ Atividades/capitulo3/testes.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
-
-int main()
-{
-    int i = 0;
-    do 
-    {
-        printf("O mundo e legal\n");
-        printf("oi naldo\n");
-        i++;
+int main() {
+    int n, r;
+    int combinacoes = 1;
+    printf("Digite o valor de n: ");
+    scanf("%d", &n);
+    printf("Digite o valor de r: ");
+    scanf("%d", &r);
+    if (n >= 0 && r >= 0 && r <= n) {
+    // Calcula nCr diretamente usando uma abordagem iterativa
+        for (int i = 1; i <= r; i++) {
+            combinacoes *= n - i + 1;
+            combinacoes /= i;
+        }
+        printf("O valor de %dC%d eh %d\n", n, r, combinacoes);
+    } else {
+        printf("Erro: n e r devem ser maiores ou iguais a zero e r deve ser menor ou igual a n\n");
     }
-    while (i<5);
-    printf("%d", i);
     return 0;
 }
+
