7ba47a03c922f7f36edf8b72127e19038b59bb3b
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-10-11 14:56:44 -0300

    att

--- matriz/arq1.c
+++ matriz/arq1.c
@@ -2,7 +2,29 @@
 
 int main()
 {
-    // matriz[linha][coluna]
-    
+
+    int mat[2][2], l, c, x1 = 1, x2 = 1;
+    for (l = 0; l < 2; l++)
+    {
+        for (c = 0; c < 2; c++)
+        {
+           scanf("%d", &mat[l][c]);
+        }
+    }
+
+    for (l = 0; l < 2; l++)
+    {
+        for (c = 0; c < 2; c++)
+        {
+            if (l == c)
+            {
+                x1 *= mat[l][c];
+            } else {
+                x2 *= mat[l][c];
+            }
+            
+        }
+    }
+    printf("O determinante eh: %d\n", x1-x2);
     return 0;
 }
