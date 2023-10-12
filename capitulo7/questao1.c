1198b07ce10e257be36e7c68ed646e97397f2a47
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-10-12 15:43:30 -0300

    att

---
+++ capitulo7/questao1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int mat[25];
+    printf("%d", mat[4]);
+    printf("%d", mat[5]);
+
+    return 0;
+}
---
+++ capitulo7/questao7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define tam 5
+int main()
+{
+    int mat[tam];
+    for (int i = 0; i <= tam; i++)
+    {
+        scanf("%d", &mat[i]);
+    }
+
+    for (int i = 0; i < tam; i++)
+    {
+        printf("%d ", mat[i]);
+    }
+    return 0;
+}
