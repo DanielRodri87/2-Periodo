88b24d8b89146dfd7761b819fa67cb90680e9358
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-11-09 08:35:09 -0300

    att

--- TopDown/aula2.c
+++ TopDown/aula2.c
@@ -18,9 +18,40 @@ void buscasequencial(int v[], int tam, int valor)
         if (v[i] == valor)
         {
             encontrou = i;
+            break;
         } else
         {
-            
+            encontrou = -1;
         }
     }
+    if (encontrou >= 0)
+    {
+        printf("Valor encontrado na posição: %d", encontrou);
+    } else
+    {
+        printf("Valor não encontrado");
+    }
+}
+
+int main()
+{
+    int val;
+    while (1)
+    {
+
+        int t;
+        printf("Digite o tamanho do vetor: ");
+        scanf("%d", &t);
+        int vetor[t];
+        preencher(vetor, t);
+        printf("Digite o valor que você deseja buscar: ");
+        scanf("%d", &val);
+        buscasequencial(vetor, t, val);
+        break;
+    
+    
+    return 0;
+        
+    }
+    
 }
