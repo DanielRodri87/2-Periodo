21a7393c7671f83c80979c6d17e82e5f3834a62f
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-11-10 00:20:58 -0300

    att

---
+++ Lista2/questao1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int vA[100], vB[100], op;
+
+    printf("MENU: 1 - Maior Elemento do Vetor A\n2 - Menor elemento do Vetor B\n3 - Calcular a SOMA e a MÉDIA dos elementos do Vetor A\n4 - Calcular a SOMA e a MÉDIA dos elementos do Vetor B\n5-  Montar o Vetor C, que é a soma dos vetores A e B e escrever os elementos do Vetor C\n6 - Escrever os elementos do Vetor A\n7 - Escrever os elementos do Vetor B\n8 - Sair:\n-> ");
+    scanf("%d", &op);
+
+    switch (op)
+    {
+    case 1:
+        break;
+    case 2: 
+        break;
+    case 3:
+        break;
+    case 4: 
+    
+        break;
+    case 5:
+        break;
+    case 6: 
+        break;
+    case 7:
+        break;
+    case 8: 
+        break;
+    default:
+        printf("Código Inválido\n");
+        break;
+    }
+
+
+    return 0;
+}
