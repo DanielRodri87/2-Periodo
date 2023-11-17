36a4268e856923c61b84ca9a72a583e64bcb87a3
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-11-16 22:16:12 -0300

    att com erros

--- Ponteiros/Lista1/questao2.c
+++ Ponteiros/Lista1/questao2.c
@@ -1,14 +1,33 @@
-char palavra[50], nova_palavra[50];
-
 #include <stdio.h>
+#include <string.h>
 
-void polindromo(char *palavra)
+char palavra[50], nova_palavra[50], boolean = 0;
+
+void palindromo(char *palavra)
 {
-    
+    for (int j = 0, i = strlen(palavra); i >= 0; i--, j++)
+    {
+        nova_palavra[j] = palavra[i];
+    }
+
+    if (strcmp(palavra, nova_palavra) == 0)
+    {
+        boolean = 1;
+    }
 }
 
 int main()
 {
+    printf("Digite uma palavra para verficar se é palindromo: ");
+    scanf("%s", palavra);
+    palindromo(palavra);
 
+    if (boolean != 0)
+    {
+        printf("A palavra é um palindromo!");
+    } else
+    {
+        printf("A palavra não é um palindromo | %s |", nova_palavra);
+    }
     return 0;
 }
