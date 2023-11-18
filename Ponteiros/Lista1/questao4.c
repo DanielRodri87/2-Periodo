47cc37d2cd2eeaee691e09da4f972d28fc4eea20
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-11-18 09:36:28 -0300

    att

--- Ponteiros/Lista1/questao4.c
+++ Ponteiros/Lista1/questao4.c
@@ -1,28 +1,42 @@
 #include <stdio.h>
 
-// 4.	Escreva um programa em C que inverta a ordem dos elementos de um array utilizando ponteiros.
-int tam, i, saida;
-
 void inverter(int *vet, int tam)
 {
-    int maior = *vet;  
-    for (int i = 1, j = tam; i < tam; i++, j--)
-    {   
+    int *inicio = vet;         
+    int *fim = vet + tam - 1;  
 
-        
+    while (inicio < fim)
+    {
+        int temp = *inicio;
+        *inicio = *fim;
+        *fim = temp;
+
+        inicio++;
+        fim--;
     }
 }
+
 int main()
 {
+    int tam;
+
     printf("Digite o tamanho do vetor: ");
     scanf("%d", &tam);
 
     int vetor[tam];
 
-    for (i = 0; i < tam; i++)
+    for (int i = 0; i < tam; i++)
     {
         printf("Digite o valor do %d° elemento do vetor: ", i + 1);
         scanf("%d", &vetor[i]);
+    }
+
+    inverter(vetor, tam);
+
+    printf("Vetor invertido: ");
+    for (int i = 0; i < tam; i++)
+    {
+        printf("%d ", vetor[i]);
     }
 
     
