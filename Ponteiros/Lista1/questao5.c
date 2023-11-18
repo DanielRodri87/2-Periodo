b56188ee7f8665113d638d322bf6aa2c3fefef84
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-11-18 16:50:25 -0300

    questao 5 alan

--- Ponteiros/Lista1/questao5.c
+++ Ponteiros/Lista1/questao5.c
@@ -6,26 +6,24 @@ void char_repete(char *l, char *pal)
     int i, count = 0;
     for (i = 0; i < strlen(pal); i++)
     {
-        if (pal[i] == l)
+        if (pal[i] == *l)
         {
             count++;
         }
     }
-
-    printf("A letra %c se repete: %d", l, count);
+    printf("A letra %c se repete: %d\n", *l, count);
 }
-
 
 int main()
 {
     char letra, palavra[50];
 
     printf("Informe uma letra: ");
-    scanf("%c", &letra);
+    scanf(" %c", &letra); 
     printf("Informe uma palavra: ");
     scanf("%s", palavra);
 
-    char_repete(letra, palavra);
+    char_repete(&letra, palavra);
 
     return 0;
 }
